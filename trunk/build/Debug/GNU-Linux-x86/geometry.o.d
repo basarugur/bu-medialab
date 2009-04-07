2bd11da9bc682ea8b6fcaa97f5bf9d64a8ae7a5a
ozan.cetinaslan <ozan.cetinaslan@19b6b63a-0a50-11de-97d8-e990b0f60ea0>
2009-04-07 17:16:38 +0000

    Stereo application with fog, grid and multiviewport effects

---
+++ trunk/build/Debug/GNU-Linux-x86/geometry.o.d
@@ -0,0 +1,5 @@
+build/Debug/GNU-Linux-x86/geometry.o: geometry.cpp geometry.h vrml_io.h
+
+geometry.h:
+
+vrml_io.h:
---
+++ trunk/build/Debug/GNU-Linux-x86/vrml_io.o.d
@@ -0,0 +1,5 @@
+build/Debug/GNU-Linux-x86/vrml_io.o: vrml_io.cpp vrml_io.h geometry.h
+
+vrml_io.h:
+
+geometry.h:
